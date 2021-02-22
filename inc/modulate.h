6fc9dcd4ddb102c9527821e14f643618b1028e23
Max-Beadnell <78924930+Max-Beadnell@users.noreply.github.com>
2021-02-22 17:21:42 +0000

    added modulate function class file

---
+++ inc/modulate.h
@@ -0,0 +1,13 @@
+/*
+ * modulate.h
+ *
+ *  Created on: 22 Feb 2021
+ *      Author: maxbeadnell
+ */
+
+#ifndef INC_MODULATE_H_
+#define INC_MODULATE_H_
+
+
+
+#endif /* INC_MODULATE_H_ */
---
+++ src/modulate.c
@@ -0,0 +1,18 @@
+/*
+ * modulate.c
+ *
+ *  Created on: 22 Feb 2021
+ *      Author: maxbeadnell
+ */
+
+#include "main.h"
+
+float modulate(uint16_t carrier, uint16_t modulator, float amount){
+	if(amount == 1.0){return 1;}
+
+
+
+
+}
+
+
